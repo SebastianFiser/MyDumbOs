7f46c01dbd7ab6a93aafade5003fd6ba1e207d9d
SebastianFiser <sebastianfiser@gmail.com>
2025-10-04 19:31:16 +0200

    pracovní možná funkční verze

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xue
+QEMU=qemu-system-riscv32
+
+CC=/usr/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+
+$CC $CFLAGS -W1,-Tkernel.ld -W1,-Map=kernel.map -o kernel.elf \ 
+	kernel.c
+
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+	-kernel kernel.elf
