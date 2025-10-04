c9ccae74cc532fb9baa704604a112fdf68489c5d
SebastianFiser <sebastianfiser@gmail.com>
2025-10-04 20:22:53 +0200

    Attempt on bug fixes

--- run.sh
+++ run.sh
@@ -2,12 +2,10 @@
 set -xue
 QEMU=qemu-system-riscv32
 
-CC=/usr/bin/clang
+CC="/usr/bin/clang"
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
-$CC $CFLAGS -W1,-Tkernel.ld -W1,-Map=kernel.map -o kernel.elf \ 
-	kernel.c
+$CC $CFLAGS -W1,-Tkernel.ld -W1,-Map=kernel.map -o kernel.elf \kernel.c
 
 
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-	-kernel kernel.elf
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \-kernel kernel.elf
