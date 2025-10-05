6f4bad86e196fbdce7549f4a304cca1f7c10bc02
SebastianFiser <sebastianfiser@gmail.com>
2025-10-05 20:55:17 +0200

    Funkční výpis fráze hello world

--- kernel.c
+++ kernel.c
@@ -1,8 +1,13 @@
-#include "kernel.h"
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
 
-struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long  fid, long eid) {
+#include "kernel.h"
+
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
+                       long arg5, long fid, long eid) {
     register long a0 __asm__("a0") = arg0;
     register long a1 __asm__("a1") = arg1;
     register long a2 __asm__("a2") = arg2;
@@ -14,23 +19,41 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, lo
 
     __asm__ __volatile__("ecall"
                          : "=r"(a0), "=r"(a1)
-                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                           "r"(a6), "r"(a7)
                          : "memory");
     return (struct sbiret){.error = a0, .value = a1};
-} 
+}
 
 void putchar(char ch) {
-    sbi_call(ch, 0,0,0,0,0,0,1 /*console putchar*/);
-
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
 
 void kernel_main(void) {
-    const char *s = "Hello, World from MyDumbOS!\n";
+    const char *s = "\n\nHello World from MyDumbOs!\n";
     for (int i = 0; s[i] != '\0'; i++) {
         putchar(s[i]);
     }
 
-    for(;;) {
+    for (;;) {
         __asm__ __volatile__("wfi");
     }
+}
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+void boot(void) {
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n" // Set the stack pointer
+        "j kernel_main\n"       // Jump to the kernel main function
+        :
+        : [stack_top] "r" (__stack_top) // Pass the stack top address as %[stack_top]
+    );
 }
