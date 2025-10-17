f1a50ff5def95886b77cb48c441622ddabf6ff2a
SebastianFiser <sebastianfiser@gmail.com>
2025-10-17 18:17:25 +0200

    integrated cpu table

--- kernel.h
+++ kernel.h
@@ -1,12 +1,24 @@
+#include "common.h"
+#define SATP_SV32 (1u << 31)
+#define PAGE_V    (1 << 0)   // "Valid" bit (entry is enabled)
+#define PAGE_R    (1 << 1)   // Readable
+#define PAGE_W    (1 << 2)   // Writable
+#define PAGE_X    (1 << 3)   // Executable
+#define PAGE_U    (1 << 4)   // User (accessible in user mode)
+
 #pragma once
 #define PANIC(fmt, ...) \
     do { \
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
         while (1) {} \
     } while (0)
-
-#include "common.h"
-
+struct process {
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint32_t *page_table;
+    uint8_t stack[8192];
+};
 struct trap_frame {
     uint32_t ra;
     uint32_t gp;
@@ -58,13 +70,6 @@ struct trap_frame {
 
 #define PROC_UNUSED   0   // Unused process control structure
 #define PROC_RUNNABLE 1   // Runnable process
-
-struct process {
-    int pid;             // Process ID
-    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
-    vaddr_t sp;          // Stack pointer
-    uint8_t stack[8192]; // Kernel stack
-};
 
 struct sbiret {
     long error;
